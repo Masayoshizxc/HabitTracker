995131c09183f6373b32e46bb918b16b17fa920f
Adilet <masayoshizxc@gmail.com>
2023-05-12 20:29:45 +0600

    Auth

--- HabitsTracker/AppDelegate.swift
+++ HabitsTracker/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import FirebaseCore
 import GoogleSignIn
 
 @main
@@ -15,8 +16,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+//        #MARK: Google Sign In
+        
+        
         return true
+    }
+    
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
     }
 
     // MARK: UISceneSession Lifecycle
